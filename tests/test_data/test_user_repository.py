c48d2aec854ad97da66e46f0332f692b2332b8bd
Matt @ Ace <matt@ace.do.not.use>
2026-02-11 16:49:01 -0700

    added more assertions to increase mutation score to 65

--- tests/test_data/test_user_repository.py
+++ tests/test_data/test_user_repository.py
@@ -20,16 +20,47 @@ class TestUserRepository(unittest.TestCase):
         self.assertIsNotNone(user)
         self.assertEqual(user['id'], 1)
         self.assertEqual(user['name'], 'Alice')
+        self.assertEqual(user['email'], 'alice@example.com')
+
+    def test_get_by_id_existing_user2(self):
+        user = self.repo.get_by_id(2)
+        self.assertIsNotNone(user)
+        self.assertEqual(user['id'], 2)
+        self.assertEqual(user['name'], 'Bob')
+        self.assertEqual(user['email'], 'bob@example.com')
 
     def test_get_by_id_nonexistent_user(self):
         user = self.repo.get_by_id(999)
         self.assertIsNone(user)
+
+    def test_create_users(self):
+        new_user = self.repo.create({
+            'name': 'Charlie',
+            'email': 'charlie@example.com'
+        })
+        self.assertEqual(new_user['id'], 3)
+        self.assertEqual(new_user['name'], 'Charlie')
+        self.assertEqual(new_user['email'], 'charlie@example.com')
+
+        new_user = self.repo.create({
+            'name': 'Bill',
+            'email': 'bill@example.com'
+        })
+        self.assertEqual(new_user['id'], 4)
+        self.assertEqual(new_user['name'], 'Bill')
+        self.assertEqual(new_user['email'], 'bill@example.com')
 
     def test_create_user(self):
         new_user = self.repo.create({
             'name': 'Charlie',
             'email': 'charlie@example.com'
         })
-        self.assertIsNotNone(new_user['id'])
+        self.assertEqual(new_user['id'], 3)
         self.assertEqual(new_user['name'], 'Charlie')
         self.assertEqual(new_user['email'], 'charlie@example.com')
+
+        user = self.repo.get_by_id(3)
+        self.assertIsNotNone(user)
+#        self.assertEqual(user['id'], 2)
+#        self.assertEqual(user['name'], 'Bob')
+#        self.assertEqual(user['email'], 'bob@example.com')
